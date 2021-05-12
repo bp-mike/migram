8ce9c6d4a79778c6b5e002a189f6326c167776e9
bp mike <bpmikeomo@gmail.com>
2021-05-12 18:08:37 +0300

    modify procfile

--- db.js
+++ db.js
@@ -2,8 +2,9 @@ const dotenv = require('dotenv')
 dotenv.config()
 const mongodb = require('mongodb')
 
-mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
+
+mongodb.connect(process.env.CONNECTIONSTRING, {userNewUrlParser: true, useUnifiedTopology: true},(err, client) =>{
   module.exports = client
   const app = require('./app')
-  app.listen(process.env.PORT)
+  app.listen(process.env.port)
 })
